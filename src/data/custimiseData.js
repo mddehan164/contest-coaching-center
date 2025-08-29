5b6c5807f33a6b5fa4163e9338ce7b35b5d1e2f7
MD Dehan <mddehan164@gmail.com>
2025-08-29 19:03:54 +0600

    remove course from customise

--- src/data/custimiseData.js
+++ src/data/custimiseData.js
@@ -1,22 +1,48 @@
 import { TfiLayoutSliderAlt } from "react-icons/tfi";
 import { AiOutlineNotification } from "react-icons/ai";
-import { FaGraduationCap } from 'react-icons/fa';
-import { FaAward } from 'react-icons/fa';
+import { FaGraduationCap } from "react-icons/fa";
+import { FaAward } from "react-icons/fa";
 import { ImStatsDots } from "react-icons/im";
 import { MdSupportAgent } from "react-icons/md";
 import { MdOutlineSummarize } from "react-icons/md";
 import { GiTreeBranch } from "react-icons/gi";
 import { GrGallery } from "react-icons/gr";
 
-
 export const customiseData = [
-    {icon: TfiLayoutSliderAlt, name:"Add/Edit current Slider", link:"slider"},
-    {icon: AiOutlineNotification, name:"Add/Edit Existing Notice data", link:"notice"},
-    {icon: FaGraduationCap, name:"Add/Edit current Courses", link:"course-editor"},
-    {icon: ImStatsDots, name:"Add/Edit current Stats", link:"success-editor"},
-    {icon: FaAward, name:"Add/Edit current Successful persons data", link:"stats-editor"},
-    {icon: MdSupportAgent, name:"Add/Edit current Service data", link:"service-editor"},
-    {icon: MdOutlineSummarize, name:"Edit current Summary data", link:"summary-editor"},
-    {icon: GiTreeBranch, name:"Add/Edit current Branch data", link:"branch-editor"},
-    {icon: GrGallery, name:"Add/Edit current Gallery data", link:"gallery-editor"},
-]
+  { icon: TfiLayoutSliderAlt, name: "Add/Edit current Slider", link: "slider" },
+  {
+    icon: AiOutlineNotification,
+    name: "Add/Edit Existing Notice data",
+    link: "notice",
+  },
+  {
+    icon: ImStatsDots,
+    name: "Add/Edit current Successfull person",
+    link: "success-editor",
+  },
+  {
+    icon: FaAward,
+    name: "Add/Edit current stats data",
+    link: "stats-editor",
+  },
+  {
+    icon: MdSupportAgent,
+    name: "Add/Edit current Service data",
+    link: "service-editor",
+  },
+  {
+    icon: MdOutlineSummarize,
+    name: "Edit current Summary data",
+    link: "summary-editor",
+  },
+  {
+    icon: GiTreeBranch,
+    name: "Add/Edit current Branch data",
+    link: "branch-editor",
+  },
+  {
+    icon: GrGallery,
+    name: "Add/Edit current Gallery data",
+    link: "gallery-editor",
+  },
+];
